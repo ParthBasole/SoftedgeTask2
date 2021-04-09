a682a9aec3d1aa8dd1d5943343bf0359c7729fec
ParthB <41703901+ParthBasole@users.noreply.github.com>
2021-04-08 21:30:57 -0700

    Add files via upload

--- Task2.cpp
+++ Task2.cpp
@@ -5,6 +5,9 @@
 
 #include <opencv2/opencv.hpp>
 
+using namespace std;
+using namespace cv;
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     cout<<"insufficient argument number";
@@ -13,20 +16,19 @@ int main(int argc, char* argv[]) {
   
   cv::CascadeClassifier classifier(argv[1]);
 
-  
 
-  cv::namedWindow("Win1", CV_WINDOW_AUTOSIZE);
+//  cv::namedWindow("Win1", CV_WINDOW_AUTOSIZE);
 
-  cv::VideoCapture capture(0); 
+//  cv::VideoCapture capture(0); 
 
-  cv::Mat image;
+  //cv::Mat image;
   cv::Mat grayscale_image;
-  Mat image = imread("image.jpg", IMREAD_GRAYSCALE);
-  
+  cv::Mat image = imread("/content/drive/MyDrive/OPENCV_C++_HARCASCADE/pexels-photo-614810.jpeg");
+    
     std::vector<cv::Rect> features;
 
     cv::cvtColor(image, grayscale_image, CV_BGR2GRAY);
-    cv::equalizeHist(grayscale_image, grayscale_image);
+    //cv::equalizeHist(grayscale_image, grayscale_image);
 
     classifier.detectMultiScale(grayscale_image, features, 1.1, 2,
                                 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
@@ -37,17 +39,19 @@ int main(int argc, char* argv[]) {
     }
 
     
-    cv::imshow(window_name, image);
+    //cv::imshow("Win1", image);
 
     
-    switch (cv::waitKey(10)) {
+   /* switch (cv::waitKey(10)) {
       case 'q':
         std::exit(EXIT_SUCCESS);
       case 'Q':
         std::exit(EXIT_SUCCESS);
       default:
         break;
-    }
+    }*/
+
+    cv::imwrite("/content/Output/ssd.jpg",image);
   //}
   return EXIT_SUCCESS;
 }
